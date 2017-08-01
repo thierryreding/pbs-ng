c0a026ca603bc20e721ad0fda56ffff7d31457b7
Thierry Reding <thierry.reding@gmail.com>
2017-08-01 16:57:36 +0200

    Add build-tools/meson package
    Also add a build-tools/ninja package required by meson, as well as
    support files to help build packages using meson.
    
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

---
+++ packages/meson.mk
@@ -0,0 +1,31 @@
+include $(TOP_SRCDIR)/packages/common.mk
+
+$(builddir):
+	mkdir -p $@
+
+$(builddir)/cross-compile.txt: $(TOP_SRCDIR)/support/cross-compile.meson | $(builddir)
+	sed 's|@OS@|$(OS)|;s|@ARCH@|$(ARCH)|;s|@CPU@|$(CPU)|;s|@ENDIAN@|$(ENDIAN)|;s|@HOST@|$(HOST)|;s|@SYSROOT@|$(SYSROOT)|' $< > $@
+
+env = \
+	PKG_CONFIG_LIBDIR='$(SYSROOT)$(PREFIX)/lib/pkgconfig:$(SYSROOT)$(PREFIX)/share/pkgconfig' \
+	PKG_CONFIG_SYSROOT_DIR='$(SYSROOT)' \
+	CPPFLAGS='$(CPPFLAGS)' \
+	CXXFLAGS='$(CXXFLAGS)' \
+	LDFLAGS='$(LDFLAGS)' \
+	CFLAGS='$(CFLAGS)' \
+	DESTDIR=$(DESTDIR)
+
+$(builddir)/stamp-configure: $(builddir)/cross-compile.txt
+	$(env) meson --cross-file $< --prefix $(PREFIX) $(srcdir) $(builddir)
+	touch $@
+
+$(builddir)/stamp-build: $(builddir)/stamp-configure
+	$(env) ninja -C $(builddir)
+	touch $@
+
+$(builddir)/stamp-install: $(builddir)/stamp-build
+	$(env) ninja -C $(builddir) install
+
+install: $(builddir)/stamp-install
+
+.PHONY: install
