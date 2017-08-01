d3b7e790158d6d3c398e7add7bc49fc569381999
Thierry Reding <thierry.reding@gmail.com>
2017-08-01 16:52:48 +0200

    Add build-tools/cmake package
    Also add support files to help build packages that use cmake.
    
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

---
+++ packages/cmake.mk
@@ -0,0 +1,40 @@
+include $(TOP_SRCDIR)/packages/common.mk
+
+$(builddir):
+	mkdir -p $@
+
+$(builddir)/cross-compile.cmake: $(TOP_SRCDIR)/support/cross-compile.cmake.in | $(builddir)
+	sed 's|@HOST@|$(HOST)|;s|@SYSROOT@|$(SYSROOT)|' $< > $@
+
+env = \
+	PKG_CONFIG_LIBDIR='$(SYSROOT)$(PREFIX)/lib/pkgconfig:$(SYSROOT)$(PREFIX)/share/pkgconfig' \
+	PKG_CONFIG_SYSROOT_DIR='$(SYSROOT)'
+
+cmake-args = \
+	-DCMAKE_TOOLCHAIN_FILE=$(builddir)/cross-compile.cmake \
+	-DCMAKE_INSTALL_PREFIX=$(PREFIX)
+
+$(builddir)/stamp-configure: | $(builddir)/cross-compile.cmake
+	cd $(builddir) && \
+		$(env) cmake $(cmake-args) $(srcdir)
+	touch $@
+
+build-args = \
+	-j $(JOBS)
+
+$(builddir)/stamp-build: $(builddir)/stamp-configure
+	cd $(builddir) && \
+		$(env) $(MAKE) $(build-args)
+	touch $@
+
+install-args = \
+	DESTDIR='$(DESTDIR)'
+
+$(builddir)/stamp-install: $(builddir)/stamp-build
+	cd $(builddir) && \
+		$(env) $(MAKE) $(install-args) install
+	touch $@
+
+install: $(builddir)/stamp-install
+
+.PHONY: install
