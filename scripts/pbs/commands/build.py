9dc580811127e55965b6a5e71cdcc6ff03d1bd07
Thierry Reding <thierry.reding@gmail.com>
2017-08-03 15:23:07 +0200

    pbs: build: Add --force and --incremental options
    The --force option can be used to force building a package, while
    --incremental is used to build from existing source and build
    directories, rather than rebuilding from scratch.
    
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- scripts/pbs/commands/build.py
+++ scripts/pbs/commands/build.py
@@ -1,18 +1,26 @@
+import argparse
+
 description = 'build package(s)'
 usage = 'build [options] package [package...]'
 summary = ''
 
 def exec(project, *args):
-    if len(args) > 1:
-        for name in args[1:]:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--force', '-f', action = 'store_true')
+    parser.add_argument('--incremental', '-i', action = 'store_true')
+    parser.add_argument('packages', nargs = '*')
+    args = parser.parse_args(args[1:])
+
+    if len(args.packages):
+        for name in args.packages:
             package = project.find_package(name)
             if not package:
                 print('ERROR: package', name, 'not found')
                 continue
 
-            package.build()
+            package.build(args.force, args.incremental)
     else:
         for package in project.packages:
-            package.build()
+            package.build(args.force, args.incremental)
 
 # vim: et sts=4 sw=4 ts=4
