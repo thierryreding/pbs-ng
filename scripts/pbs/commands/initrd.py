4dc8203b21bb5e110159c0bd5d2c2ecc817e886f
Thierry Reding <thierry.reding@gmail.com>
2017-08-10 19:41:27 +0200

    pbs: Add initrd command
    This new command uses the mkinitrd package along with any hooks by other
    packages installed in the target filesystem to create an initial ramdisk
    that can be used to boot the system.
    
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

---
+++ scripts/pbs/commands/initrd.py
@@ -0,0 +1,45 @@
+import argparse
+import os.path
+import pbs
+import shutil
+import subprocess
+
+description = 'build initial ramdisk'
+usage = 'initrd [options]'
+summary = ''
+
+def exec(project, *args):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--output', '-o')
+    args = parser.parse_args(args[1:])
+
+    sysroot = os.path.join(pbs.objtree, 'sysroot')
+    initrd = os.path.join(pbs.objtree, 'initrd')
+    mkinitrd = os.path.join(sysroot, 'usr/bin/mkinitrd')
+
+    if not os.path.exists(mkinitrd):
+        pbs.log.error('%s not found' % mkinitrd)
+        return
+
+    if os.path.exists(initrd):
+        pbs.log.begin('removing %s...' % initrd)
+        shutil.rmtree(initrd)
+        pbs.log.end('done')
+
+    if args.output:
+        output = args.output
+    else:
+        output = 'initrd.gz'
+
+    command = [ mkinitrd, '--output', output, sysroot, initrd ]
+
+    pbs.log.print('creating initial ramdisk:', ' '.join(command))
+
+    with subprocess.Popen(command, stdout=subprocess.PIPE,
+                          stderr=subprocess.STDOUT) as proc:
+        while True:
+            line = proc.stdout.readline()
+            if not line:
+                break
+
+            pbs.log.print(line.decode().strip())
