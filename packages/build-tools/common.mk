74af52b172fe8878fd67bb677a4a28087e7ce81d
Thierry Reding <thierry.reding@gmail.com>
2019-11-02 17:42:44 +0100

    build-tools: Export LD_RUN_PATH environment variable
    The LD_RUN_PATH environment variable is used by the linker to embed the
    path to shared libraries in executables. Specifying this makes sure that
    build tools are linked against libraries in the build-tools directory
    first. This in turn helps harden the build tools against changes on the
    build system.
    
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- packages/build-tools/common.mk
+++ packages/build-tools/common.mk
@@ -4,6 +4,7 @@ BUILD_TOOLS = $(TOP_OBJDIR)/build-tools
 SYSROOT = $(TOP_OBJDIR)/sysroot
 
 ACLOCAL_PATH := $(BUILD_TOOLS)/share/aclocal
+LD_RUN_PATH := $(BUILD_TOOLS)/lib:$(LD_RUN_PATH)
 PATH := $(BUILD_TOOLS)/bin:$(PATH)
 
 PKG_CONFIG_LIBDIR := $(BUILD_TOOLS)/lib/pkgconfig
@@ -13,4 +14,4 @@ srcdir = $(CURDIR)/source
 
 JOBS ?= $(NUM_CPUS)
 
-export ACLOCAL_PATH PKG_CONFIG_LIBDIR
+export ACLOCAL_PATH LD_RUN_PATH PKG_CONFIG_LIBDIR
