fbf3ae92b52fd1e8a86e25fb62c7172782aebc49
Thierry Reding <thierry.reding@gmail.com>
2022-12-06 14:05:45 +0100

    build-tools: Export LD_LIBRARY_PATH
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- packages/build-tools/common.mk
+++ packages/build-tools/common.mk
@@ -2,9 +2,16 @@ include config.mk
 
 BUILD_TOOLS = $(TOP_OBJDIR)/build-tools
 SYSROOT = $(TOP_OBJDIR)/sysroot
+#
+# Note that this is the prefix on the host, in case we ever need it during
+# the build process of build tools. BUILD_TOOLS is the correct prefix to use
+# on the build system.
+#
+PREFIX = /usr
 
 ACLOCAL_PATH := $(BUILD_TOOLS)/share/aclocal
 LD_RUN_PATH := $(BUILD_TOOLS)/lib:$(LD_RUN_PATH)
+LD_LIBRARY_PATH := $(BUILD_TOOLS)/lib
 PATH := $(BUILD_TOOLS)/bin:$(PATH)
 
 PKG_CONFIG_LIBDIR := $(BUILD_TOOLS)/lib/pkgconfig
@@ -14,4 +21,4 @@ srcdir = $(CURDIR)/source
 
 JOBS ?= $(NUM_CPUS)
 
-export ACLOCAL_PATH LD_RUN_PATH PKG_CONFIG_LIBDIR
+export ACLOCAL_PATH LD_RUN_PATH LD_LIBRARY_PATH PKG_CONFIG_LIBDIR
