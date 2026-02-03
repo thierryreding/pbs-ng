2d51da46073c237f4bd99a58b79bf59679256c88
Thierry Reding <thierry.reding@gmail.com>
2026-02-03 16:28:49 +0100

    packages: Refactor common Python build snippet
    Reuse the sysconfigdata snippet and collapse build and installation into
    a single step since we usually don't need to keep them separate.
    
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- packages/python.mk
+++ packages/python.mk
@@ -1,28 +1,16 @@
 include $(TOP_SRCDIR)/packages/common.mk
-
-ifeq ($(ARCH),arm64)
-  ARCH = aarch64
-endif
-
-MULTIARCH = $(ARCH)-$(OS)-$(LIBC)$(ABI)
-
-env += \
-	_PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata__$(OS)_$(MULTIARCH)
+include $(TOP_SRCDIR)/packages/python-sysconfigdata.mk
 
 env += \
 	PKG_CONFIG_LIBDIR='$(SYSROOT)$(PREFIX)/lib/pkgconfig:$(SYSROOT)$(PREFIX)/share/pkgconfig' \
 	PKG_CONFIG_SYSROOT_DIR='$(SYSROOT)'
 
-$(srcdir)/stamp-build: | $(srcdir)
-	cd $(srcdir) && $(env) python3 setup.py build $(build-args)
-	touch $@
-
 install-args += \
 	--prefix=$(PREFIX) \
 	--root=$(DESTDIR)
 
-$(srcdir)/stamp-install: $(srcdir)/stamp-build
-	cd $(srcdir) && $(env) python3 setup.py install $(install-args)
+$(srcdir)/stamp-install: | $(srcdir)
+	cd $(srcdir) && $(env) python3 -m pip install $(install-args) .
 	touch $@
 
 install: $(srcdir)/stamp-install
