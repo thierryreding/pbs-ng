a78ced889f9dcd2d9fb2e035e9acdb5c2fcef223
Thierry Reding <thierry.reding@gmail.com>
2026-06-22 11:20:58 +0200

    scripts/pbs: Add --all option to watch command
    This can be used as a quick way of getting watch information on all
    packages, not just selected ones.
    
    Obviously this is only partially helpful because unless selected, a
    package will not get any build coverage.
    
    Signed-off-by: Thierry Reding <thierry.reding@gmail.com>

--- scripts/pbs/commands/watch.py
+++ scripts/pbs/commands/watch.py
@@ -2,15 +2,25 @@ import click
 
 @click.command()
 @click.option('--verbose', '-v', is_flag = True, help = 'show verbose messages')
+@click.option('--all', '-a', is_flag = True, help = 'scan all packages')
 @click.argument('packages', nargs = -1)
 @click.pass_obj
-def command(context, verbose, packages):
+def command(context, verbose, all, packages):
     '''
     Scan for new versions of a list of packages. If no PACKAGES are specified,
     scan for new versions of all selected packages.
+
+    If the --all option is specified, scan for new versions of all packages,
+    not just selected ones.
     '''
 
-    if len(packages):
+    if all:
+        if len(packages):
+            print('WARNING: --all is specified, PACKAGES argument ignored')
+
+        for package in context.db.walk():
+            package.watch(verbose)
+    elif len(packages):
         for name in packages:
             package = context.project.find_package(name)
             if not package:
